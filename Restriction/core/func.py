6a7aafd841b7fb04369f80d5c98a4c3152953156
PRADEEP999901 <149851567+PRADEEP999901@users.noreply.github.com>
2024-06-25 19:32:26 +0530

    Update func.py

--- Restriction/core/func.py
+++ Restriction/core/func.py
@@ -33,13 +33,13 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("Sorry Sir, You are Banned. Contact -- @devggn")
+            await message.reply_text("Sorry Sir, You are Banned. Contact -- @RAJPUT681830")
             return 1
       except UserNotParticipant:
          await message.reply_photo(photo="https://graph.org/file/d44f024a08ded19452152.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
          return 1
       except Exception:
-         await message.reply_text("Something Went Wrong. Contact us @devggn...")
+         await message.reply_text("Something Went Wrong. Contact us @RAJPUT681830...")
          return 1
 
 
@@ -86,7 +86,7 @@ PROGRESS_BAR = """\n
 **__Bytes__** : {0}%
 **__Speed__** : {3}/s
 **__Time__** : {4}
-**__Powered by [Team SPY](https://t.me/devggn)__**
+**__Powered by [Team SPY](https://t.me/RAJPUT681830)__**
 """
 
 
